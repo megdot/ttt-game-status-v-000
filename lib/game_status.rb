ef46e62f6e1e94a78c2d3092f54be78e16836f39
Megan Dotto <megan.dotto@gmail.com>
2018-02-19 06:50:31 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,42 +5,51 @@ end
 
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
-  [0,1,2], #Top row
-  [3,4,5], #Middle row
-  [6,7,8], #Bottom row
-  [0,3,6], #Left column
-  [1,4,7], #Middle column
-  [2,5,8], #Right column
-  [0,4,8], #Left start diagonal
-  [2,4,6]  #Right start diagonal
-]
-
-board = ["X", "X", "X", "O", " ", "O", "O", " ", " "]
-camel = [8, " ", " ", " ", 8, " ", " ", " ", 8]
-
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ]
+#won?
 def won?(board)
-winner = []
-empty_board = board.all? {|x| x == " "}
-WIN_COMBINATIONS.each do |sub_array|
-    if empty_board || full?(board)
-      return false
-    elsif sub_array.all? { |value| board[value] =="X" } || sub_array.all? { |value| board[value] =="O" }
-      winner = sub_array
+  WIN_COMBINATIONS.detect do |win_combo|
+    if (board[win_combo[0]]) == "X" && (board[win_combo[1]]) == "X" && (board[win_combo[2]]) == "X"
+      return win_combo
+    elsif (board[win_combo[0]]) == "O" && (board[win_combo[1]]) == "O" && (board[win_combo[2]]) == "O"
+      return win_combo
+    end
+      false
+  end
+end
+
+#full?
+def full?(board)
+  board.all?{|occupied| occupied != " "}
+end
+
+#draw?
+def draw?(board)
+  !(won?(board)) && (full?(board))
+end
+
+#over?
+def over?(board)
+  (won?(board)) || (full?(board)) || (draw?(board))
+end
+
+#winner?
+def winner(board)
+  WIN_COMBINATIONS.detect do |win_combo|
+    if (board[win_combo[0]]) == "X" && (board[win_combo[1]]) == "X" && (board[win_combo[2]]) == "X"
+      return "X"
+    elsif (board[win_combo[0]]) == "O" && (board[win_combo[1]]) == "O" && (board[win_combo[2]]) == "O"
+      return "O"
+    else
+      nil
     end
   end
-  winner
 end
-
-def full?(board)
-  !board.any? { |x| x == " " }
-end
-
-def draw?(board)
-  if !won?(board) && full?(board)
-    return true
-  elsif !won?(board) && !full?(board) || !won?(board)
-    return false
-  end
-end
-
-draw?(board)
